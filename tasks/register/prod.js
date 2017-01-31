76f5f081334abd209ecd4c951f72e056b8a39d24
Youri T. K. K. Mattar <youri@youhide.com.br>
2017-01-30 23:12:01 -0200

    Revert "test prod grunt task"
    This reverts commit 4089395e1cee37432483b5e90aa1abe33ad6b2ad.

--- tasks/register/prod.js
+++ tasks/register/prod.js
@@ -17,7 +17,6 @@ module.exports = function(grunt) {
     'concat',
     'uglify',
     'cssmin',
-    'rename',
     'sails-linker:prodJs',
     'sails-linker:prodStyles',
     'sails-linker:devTpl',
