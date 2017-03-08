cdf23135945ff4e77acc3982e345c64cbf47976a
Youri T. K. K. Mattar <youri@youhide.com.br>
2017-03-08 15:11:33 -0300

    rm document ready

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,45 +16,43 @@ io.socket.get('/socket/join', function gotResponse(body, response) {
 
 // Post data to server with sockets
 
-$(document).ready(function(){
-  $('#searchForm').submit(function(event){
-    var inputVal = $('#searchInput').val();
-    var postVal = {searchValue: inputVal};
-    io.socket.post('/socket/getname', postVal, function (resData, jwRes) {
-      console.log('jwRes = '+jwRes.statusCode);
-      console.log('resData = '+resData.cards);
-      document.title = resData.title;
-      window.history.pushState('', '', '/search/'+inputVal);
-      $('#cardsList').empty();
-      if (typeof resData.cards == 'object' && resData.cards) {
-        resData.cards.forEach( function( card ){
-          $('#cardsList').append('<div class="col-md-3 col-sm-6 cardcol-'+card.id+'">');
-          $('.cardcol-'+card.id).append('<div class="box cardbox-'+card.id+'">');
-          if (card.imageUrl) {
-            $('.cardbox-'+card.id).append('<img class="card-img img-responsive center-block" src="'+card.imageUrl+'" alt="Card image cap">');
-          } else {
-            $('.cardbox-'+card.id).append('<img class="card-img img-responsive center-block" src="https://upload.wikimedia.org/wikipedia/commons/a/ac/No_image_available.svg" alt="Card image cap">');
-          }
-          $('.cardbox-'+card.id).append('<div class="cardInfo-block cardInfo-'+card.id+'">');
-          $('.cardInfo-'+card.id).append('<h4 class="card-title">'+card.name+'</h4>');
-          $('.cardInfo-'+card.id).append('<p class="card-text">Rarity: '+card.rarity+'</p>');
-          $('.cardInfo-'+card.id).append('<p class="card-text">Set: '+card.setName+'</p>');
-          $('.cardInfo-'+card.id).append('<p class="card-text">Type: '+card.type+'</p>');
-          if (card.text) {
-            $('.cardInfo-'+card.id).append('<p class="card-text"><small class="text-muted">Text: '+card.text+'</small></p>');
-          } else {
-            $('.cardInfo-'+card.id).append('<p class="card-text"><small class="text-muted">Text: None</small></p>');
-          }
-          $('#cardsList').append('</div>');
-          $('#cardsList').append('</div>');
-        });
-      }
-    });
-    event.preventDefault();
+$('#searchForm').submit(function(event){
+  var inputVal = $('#searchInput').val();
+  var postVal = {searchValue: inputVal};
+  io.socket.post('/socket/getname', postVal, function (resData, jwRes) {
+    console.log('jwRes = '+jwRes.statusCode);
+    console.log('resData = '+resData.cards);
+    document.title = resData.title;
+    window.history.pushState('', '', '/search/'+inputVal);
+    $('#cardsList').empty();
+    if (typeof resData.cards == 'object' && resData.cards) {
+      resData.cards.forEach( function( card ){
+        $('#cardsList').append('<div class="col-md-3 col-sm-6 cardcol-'+card.id+'">');
+        $('.cardcol-'+card.id).append('<div class="box cardbox-'+card.id+'">');
+        if (card.imageUrl) {
+          $('.cardbox-'+card.id).append('<img class="card-img img-responsive center-block" src="'+card.imageUrl+'" alt="Card image cap">');
+        } else {
+          $('.cardbox-'+card.id).append('<img class="card-img img-responsive center-block" src="https://upload.wikimedia.org/wikipedia/commons/a/ac/No_image_available.svg" alt="Card image cap">');
+        }
+        $('.cardbox-'+card.id).append('<div class="cardInfo-block cardInfo-'+card.id+'">');
+        $('.cardInfo-'+card.id).append('<h4 class="card-title">'+card.name+'</h4>');
+        $('.cardInfo-'+card.id).append('<p class="card-text">Rarity: '+card.rarity+'</p>');
+        $('.cardInfo-'+card.id).append('<p class="card-text">Set: '+card.setName+'</p>');
+        $('.cardInfo-'+card.id).append('<p class="card-text">Type: '+card.type+'</p>');
+        if (card.text) {
+          $('.cardInfo-'+card.id).append('<p class="card-text"><small class="text-muted">Text: '+card.text+'</small></p>');
+        } else {
+          $('.cardInfo-'+card.id).append('<p class="card-text"><small class="text-muted">Text: None</small></p>');
+        }
+        $('#cardsList').append('</div>');
+        $('#cardsList').append('</div>');
+      });
+    }
   });
-  $('#searchButton').click(function(){
-    $('#searchForm').submit();
-  });
+  event.preventDefault();
+});
+$('#searchButton').click(function(){
+  $('#searchForm').submit();
 });
 
 // $(document).ready(function(){
