a1c5f2f91ba6d48d78ce5604db3986259c3db6e8
Youri T. K. K. Mattar <youri@youhide.com.br>
2018-12-23 13:40:04 -0200

    sitemap

--- cardParser/index.js
+++ cardParser/index.js
@@ -3,9 +3,10 @@
 const fs = require('fs');
 const allCards = require('./AllCards.json');
 
-fs.writeFileSync('./sitemap.txt', '');
+fs.writeFileSync('./assets/sitemap.txt', '');
 
 Object.keys(allCards).forEach(function(data){
-  fs.appendFileSync('./sitemap.txt', "https://mtgcrack.herokuapp.com/search/"+data+"\n");
+  data = encodeURIComponent(data.trim())
+  fs.appendFileSync('./assets/sitemap.txt', "https://mtgcrack.herokuapp.com/search/"+data+"\n");
   console.log(data);
 })
